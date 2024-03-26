3b18cff365e2aae219e2f2fa8abb1131febcd58d
Mingyu Park <hello0922@cau.ac.kr>
2024-03-26 16:39:35 +0900

    2024 03 26 test

--- prac1.py
+++ prac1.py
@@ -6,12 +6,12 @@ height = float(input())
 total_length = length + width + height
 
 if total_length <= 80:
-    fee = 5000
+    fee = 50000
 elif total_length <= 100:
-    fee = 8000
+    fee = 80000
 elif total_length <= 120:
-    fee = 10000
+    fee = 100000
 elif total_length <= 160:
-    fee = 13000
+    fee = 130000
 else:
     fee = 1000000000000000
